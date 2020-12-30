ba2f9b2484778ce5e3a15d71c441750ca17561d6
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-12-29 20:39:00 -0700

    added function, styling

--- src/App.js
+++ src/App.js
@@ -9,13 +9,27 @@ console.log(API_KEY)
 
 function App() {
 
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  
+
 
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+
   }
   return (
-    <div className="app">
+    <div className="app"> 
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Search..." />
@@ -23,6 +37,12 @@ function App() {
         <div className="location-box">
           <div className="location">NY City</div>
           <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            15F
+          </div>
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
