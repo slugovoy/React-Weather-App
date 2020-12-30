0b89eb05e09d738b28d673d24cb35035c817273a
Sergey Lugovoy <serg.lugovoy.81@gmail.com>
2020-12-29 22:42:30 -0700

    claened the code

--- src/App.js
+++ src/App.js
@@ -23,40 +23,6 @@ function App() {
     }
   };
 
-  // const dateBuilder = (d) => {
-  //   let months = [
-  //     "January",
-  //     "February",
-  //     "March",
-  //     "April",
-  //     "May",
-  //     "June",
-  //     "July",
-  //     "August",
-  //     "September",
-  //     "October",
-  //     "November",
-  //     "December",
-  //   ];
-  //   let days = [
-  //     "Sunday",
-  //     "Monday",
-  //     "Tuesday",
-  //     "Wednesday",
-  //     "Thursday",
-  //     "Friday",
-  //     "Saturday",
-  //   ];
-
-  //   let day = days[d.getDay()];
-  //   let date = d.getDate();
-  //   let month = months[d.getMonth()];
-  //   let year = d.getFullYear();
-
-  //   return `${day} ${date} ${month} ${year}`;
-  // };
-
-
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
   const unixTime = currentWeather.dt;
   const currentDate = new Date(unixTime*1000);
